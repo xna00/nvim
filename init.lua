a566cbf5aad776f93caaa5c9087a2b7787e14370
xna <xna00@qq.com>
2022-11-10 10:43:12 +0800

    feat: add unocss lsp config

--- init.lua
+++ init.lua
@@ -9,12 +9,12 @@ vim.cmd [[set shiftwidth=2]]
 
 vim.cmd [[let mapleader="\<space>"]]
 
-local key_pairs = {['['] = ']', ['('] = ')', ['{'] = '}'}
+local key_pairs = { ['['] = ']', ['('] = ')', ['{'] = '}' }
 for k, v in pairs(key_pairs) do
-  local opts = {noremap = true, silent = true}
+  local opts = { noremap = true, silent = true }
   vim.api.nvim_set_keymap('i', k, k .. v .. '<ESC>i', opts)
   vim.api.nvim_set_keymap('i', v, 'getline(".")[col(".") - 1] == "' .. v ..
-                              '" ? "\\<ESC>la" : "' .. v .. '"', {expr = true})
+    '" ? "\\<ESC>la" : "' .. v .. '"', { expr = true })
 
 end
 
@@ -41,24 +41,25 @@ require('packer').startup(function()
   use 'nvim-treesitter/nvim-treesitter-textobjects'
 end)
 
--- vim.cmd [[colorscheme codedark]]
+vim.cmd [[colorscheme codedark]]
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 symbol = require('symbol')
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = {noremap = true, silent = true}
+  local opts = { noremap = true, silent = true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -67,52 +68,136 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<leader>wa',
-                 '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<leader>wr',
-                 '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<leader>wl',
-                 '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                 opts)
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+    opts)
   buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-                 opts)
+    opts)
   buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-                 opts)
+    opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>',
-                 opts)
+    opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>',
-                 opts)
-  --- 
+    opts)
+  ---
   buf_set_keymap('n', '<leader>f',
-                 '<cmd>lua vim.lsp.buf.formatting({tabSize = 2, convertTabsToSpaces = true, semicolons = "insert"})<CR>',
-                 opts)
+    '<cmd>lua vim.lsp.buf.formatting({tabSize = 2, convertTabsToSpaces = true, semicolons = "insert"})<CR>',
+    opts)
   buf_set_keymap('n', '<leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
 
-  buf_set_keymap('n', '[f','<cmd>lua symbol.prev(12, "start")<CR>', opts)
-  buf_set_keymap('n', ']f','<cmd>lua symbol.next(12, "start")<CR>', opts)
-  buf_set_keymap('n', '[F','<cmd>lua symbol.prev(12, "end")<CR>', opts)
-  buf_set_keymap('n', ']F','<cmd>lua symbol.next(12, "end")<CR>', opts)
-  buf_set_keymap('n', '[c','<cmd>lua symbol.prev(14, "start")<CR>', opts)
-  buf_set_keymap('n', ']c','<cmd>lua symbol.next(14, "start")<CR>', opts)
-  buf_set_keymap('n', '[C','<cmd>lua symbol.prev(14, "end")<CR>', opts)
-  buf_set_keymap('n', ']C','<cmd>lua symbol.next(14, "end")<CR>', opts)
-  buf_set_keymap('n', '[p','<cmd>lua symbol.prev(7, "start")<CR>', opts)
-  buf_set_keymap('n', ']p','<cmd>lua symbol.next(7, "start")<CR>', opts)
+  buf_set_keymap('n', '[f', '<cmd>lua symbol.prev(12, "start")<CR>', opts)
+  buf_set_keymap('n', ']f', '<cmd>lua symbol.next(12, "start")<CR>', opts)
+  buf_set_keymap('n', '[F', '<cmd>lua symbol.prev(12, "end")<CR>', opts)
+  buf_set_keymap('n', ']F', '<cmd>lua symbol.next(12, "end")<CR>', opts)
+  buf_set_keymap('n', '[c', '<cmd>lua symbol.prev(14, "start")<CR>', opts)
+  buf_set_keymap('n', ']c', '<cmd>lua symbol.next(14, "start")<CR>', opts)
+  buf_set_keymap('n', '[C', '<cmd>lua symbol.prev(14, "end")<CR>', opts)
+  buf_set_keymap('n', ']C', '<cmd>lua symbol.next(14, "end")<CR>', opts)
+  buf_set_keymap('n', '[p', '<cmd>lua symbol.prev(7, "start")<CR>', opts)
+  buf_set_keymap('n', ']p', '<cmd>lua symbol.next(7, "start")<CR>', opts)
 end
 
+local util = require 'lspconfig.util'
+local configs = require 'lspconfig.configs'
+configs['unocss'] =
+
+{ default_config = {
+  cmd = { 'node', '/Users/xna/unocss-language-server/out/server.js', '--stdio' },
+  -- filetypes copied and adjusted from tailwindcss-intellisense
+  filetypes = {
+    -- html
+    'aspnetcorerazor',
+    'astro',
+    'astro-markdown',
+    'blade',
+    'django-html',
+    'htmldjango',
+    'edge',
+    'eelixir', -- vim ft
+    'elixir',
+    'ejs',
+    'erb',
+    'eruby', -- vim ft
+    'gohtml',
+    'haml',
+    'handlebars',
+    'hbs',
+    'html',
+    -- 'HTML (Eex)',
+    -- 'HTML (EEx)',
+    'html-eex',
+    'heex',
+    'jade',
+    'leaf',
+    'liquid',
+    'markdown',
+    'mdx',
+    'mustache',
+    'njk',
+    'nunjucks',
+    'php',
+    'razor',
+    'slim',
+    'twig',
+    -- css
+    'css',
+    'less',
+    'postcss',
+    'sass',
+    'scss',
+    'stylus',
+    'sugarss',
+    -- js
+    'javascript',
+    'javascriptreact',
+    'reason',
+    'rescript',
+    'typescript',
+    'typescriptreact',
+    -- mixed
+    'vue',
+    'svelte',
+  },
+  on_new_config = function(new_config)
+    if not new_config.settings then
+      new_config.settings = {}
+    end
+    if not new_config.settings.editor then
+      new_config.settings.editor = {}
+    end
+    if not new_config.settings.editor.tabSize then
+      -- set tab size for hover
+      new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
+    end
+  end,
+  root_dir = function(fname)
+    return util.root_pattern('tailwind.config.js', 'tailwind.config.ts')(fname)
+        or util.root_pattern('postcss.config.js', 'postcss.config.ts')(fname)
+        or util.find_package_json_ancestor(fname)
+        or util.find_node_modules_ancestor(fname)
+        or util.find_git_ancestor(fname)
+  end,
+
+} }
+
+print(lspconfig['unocss'])
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = {
-  'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'tailwindcss'
+  'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'unocss'
 }
 local settings = {
   sumneko_lua = {
     Lua = {
-      completion = {keywordSnippet = "Disable"},
-      diagnostics = {globals = {"vim", "use"}, disable = {"lowercase-global"}},
-      runtime = {version = "LuaJIT", path = vim.split(package.path, ";")},
+      completion = { keywordSnippet = "Disable" },
+      diagnostics = { globals = { "vim", "use" }, disable = { "lowercase-global" } },
+      runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
       workspace = {
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
@@ -173,10 +258,10 @@ cmp.setup {
       end
     end
   },
-  sources = {{name = 'nvim_lsp'}, {name = 'luasnip'}}
+  sources = { { name = 'nvim_lsp' }, { name = 'luasnip' } }
 }
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "tsx" },
 
